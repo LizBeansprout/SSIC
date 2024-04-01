e044189affceeaedd00eb1a1d8c5ad9b3da22097
Boszen <6322773845@g.siit.tu.ac.th>
2024-04-01 20:12:23 +0700

    Initialize

---
+++ controller.py
@@ -0,0 +1,11 @@
+import math
+
+def importExcel(excel_path):
+    pass
+
+def processInput():
+    pass
+
+def processOuput():
+    pass
+
---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import controller
+
+def create_main_window():
+    app = tk.Tk()
+    app.title("Single-Stage Inventory Control")
+
+    app.mainloop()
+
+def main():
+    create_main_window()
+
+if __name__ == "__main__":
+    main()
