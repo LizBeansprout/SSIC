6f53ee60f908e6520167a8f1e5317825a9800f38
Boszen <6322773845@g.siit.tu.ac.th>
2024-04-14 22:15:06 +0700

    First page (wire) + test.xlsx

--- controller.py
+++ controller.py
@@ -1,6 +1,26 @@
+from tkinter import filedialog
+import openpyxl as pxl
+
 import math
 
-def importExcel(excel_path):
+
+
+def importExcel():
+    # Load Excel (not include formula, Only read as actual data)
+    file = filedialog.askopenfilename()
+    excel = pxl.load_workbook(file, data_only=True)
+
+    # Selet Sheet (This select active one, latest active)
+    data = excel.active
+    #data = excel['Sheet1']
+    
+    # Iterate over table to get value
+    for row in range(0, data.max_row):
+        for col in data.iter_cols(1, data.max_column):
+            print(col[row].value)
+    
+
+def exportReport():
     pass
 
 def processInput():
@@ -8,4 +28,3 @@ def processInput():
 
 def processOuput():
     pass
-
--- main.py
+++ main.py
@@ -1,14 +1,60 @@
 import tkinter as tk
+from tksheet import Sheet
 import controller
 
-def create_main_window():
-    app = tk.Tk()
-    app.title("Single-Stage Inventory Control")
+# Intiate tkinter entity
+app = tk.Tk()
 
-    app.mainloop()
+# Setting window size
+app.geometry("1080x720")
+app.minsize(1080,720)
+app.maxsize(1080,720)
 
+# Setting title
+app.title("Single-Stage Inventory Control")
+
+# Creating menu bar
+menu_bar = tk.Menu(app)
+app.config(menu = menu_bar)
+
+# Adding major menu
+file_menu = tk.Menu(menu_bar, tearoff = 0)
+edit_menu = tk.Menu(menu_bar, tearoff = 0)
+view_menu = tk.Menu(menu_bar, tearoff = 0)
+option_menu = tk.Menu(menu_bar, tearoff = 0)
+menu_bar.add_cascade(label = "File", menu = file_menu)
+menu_bar.add_cascade(label = "Edit", menu = edit_menu)
+menu_bar.add_cascade(label = "View", menu = view_menu)
+menu_bar.add_cascade(label = "Option", menu = option_menu)
+
+# Adding command for each major menu
+  # File menu
+file_menu.add_cascade(label = 'Something')
+file_menu.add_separator()
+file_menu.add_cascade(label = 'Exit')
+  # Edit menu
+edit_menu.add_cascade(label = 'Something')
+  # View menu
+view_menu.add_cascade(label = 'Something')
+  # Option menu
+option_menu.add_cascade(label = 'Something')
+
+# Frame management
+left_frame = tk.Frame(app, bg = "lightblue", width = 360, padx = 25, pady = 10)
+left_frame.grid(row=0, column=0)
+right_frame = tk.Frame(app, bg = "red", width = 720, padx = 10, pady = 10)
+right_frame.grid(row=0, column=1)
+
+# Adding import button
+import_button = tk.Button(left_frame, text = "Import", padx = 150, pady = 8, command = controller.importExcel)
+import_button.grid(row=0,column=0)
+
+# Adding tkSheet1
+main_sheet = Sheet(right_frame,data = [[1],[2],[3]])
+main_sheet.pack()
 def main():
-    create_main_window()
+    # Intiate mainloop
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
