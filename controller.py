bf4739a3bbe3d0b173971bf76d1b37948a2ef6d4
Boszen <6322773845@g.siit.tu.ac.th>
2024-04-16 20:20:33 +0700

    Test

--- controller.py
+++ controller.py
@@ -1,24 +1,45 @@
 from tkinter import filedialog
 import openpyxl as pxl
-
 import math
 
 
-
 def importExcel():
-    # Load Excel (not include formula, Only read as actual data)
-    file = filedialog.askopenfilename()
-    excel = pxl.load_workbook(file, data_only=True)
+    try:
+        # Load Excel (not include formula, Only read as actual data)
+        file = filedialog.askopenfilename()
+        excel = pxl.load_workbook(file, data_only=True)
 
-    # Selet Sheet (This select active one, latest active)
-    data = excel.active
-    #data = excel['Sheet1']
+        # Selet Sheet (This select active one, latest active)
+        data = excel.active
+        #data = excel['Sheet1']
+
+        # Iterate over table to get value
+        for row in range(0, data.max_row):
+            for col in data.iter_cols(1, data.max_column):
+                print(col[row].value)
+    except:
+        print("Err")
+    finally:
+        pass
     
-    # Iterate over table to get value
-    for row in range(0, data.max_row):
-        for col in data.iter_cols(1, data.max_column):
-            print(col[row].value)
-    
+def isAnyData(sheet):
+    data = sheet.get_sheet_data()
+    for row in data:
+        for cell in row:
+            if cell != '':
+                print("Data in Table!")
+                return True
+    print("None in Table!")
+    return False
+
+def analyze(sheet):
+    if isAnyData(sheet):
+        try:
+            pass
+        except:
+            print("Err")
+    else:
+        print("Import your data or filled the table")
 
 def exportReport():
     pass
--- main.py
+++ main.py
@@ -40,18 +40,53 @@ view_menu.add_cascade(label = 'Something')
 option_menu.add_cascade(label = 'Something')
 
 # Frame management
-left_frame = tk.Frame(app, bg = "lightblue", width = 360, padx = 25, pady = 10)
-left_frame.grid(row=0, column=0)
-right_frame = tk.Frame(app, bg = "red", width = 720, padx = 10, pady = 10)
-right_frame.grid(row=0, column=1)
+left_frame = tk.Frame(app, bg = "lightblue",padx = 25, pady = 8)
+left_frame.grid(row=0, column=0, sticky="n")
+right_frame = tk.Frame(app, bg = "red")
+right_frame.grid(row=0, column=1, sticky="n")
 
 # Adding import button
-import_button = tk.Button(left_frame, text = "Import", padx = 150, pady = 8, command = controller.importExcel)
-import_button.grid(row=0,column=0)
+import_button = tk.Button(left_frame, text = "Import",width = 50, height = 2, command = controller.importExcel)
+import_button.grid(row=0, column=0, pady=(0,8) )
 
 # Adding tkSheet1
-main_sheet = Sheet(right_frame,data = [[1],[2],[3]])
-main_sheet.pack()
+initial_sheet = Sheet(right_frame,
+              #headers=["Row", "Column"],  # Show row and column headers
+              page_up_down_select_row=True,  # Allow selecting rows using page up/down keys
+              empty_vertical = 0,  # Prevents empty space between headers and first row
+              empty_horizontal = 0,  # Prevents empty space between headers and first column
+              column_width=120,  # Width of columns
+              row_index_width=60,  # Width of the row index column
+              total_rows=5000,  # Total number of rows
+              total_columns=100,
+              width = 670,
+              height = 720
+              )
+
+initial_sheet.enable_bindings((
+                       "toggle_select",
+                       "select_all",  # Select all cells with Ctrl+A,
+                       "drag_select",
+                       "copy",  # Copy selected cells with Ctrl+C
+                       "cut",  # Cut selected cells with Ctrl+X
+                       "paste",  # Paste copied/cut cells with Ctrl+V
+                       "undo",  # Undo action with Ctrl+Z
+                       "redo",  # Redo action with Ctrl+Y
+                       "resize",  # Resize column/row with right mouse button
+                       "column_select",  # Select entire column with Ctrl+left mouse button
+                       "row_select",  # Select entire row with Ctrl+left mouse button
+                       "external_drag_drop",  # Enable external drag and drop
+                       "box_select",  # Select rectangular area with Ctrl+drag
+                       "open_editor",  # Open editor with double click or Enter key
+                       "delete",  # Delete selected cells with Delete or Backspace key
+                       "deselect",  # Deselect cells with Esc key
+                       "edit_cell"
+                       ))
+initial_sheet.pack()
+
+analyze_button = tk.Button(left_frame, text = "Analyze",width = 50, height = 2, command =lambda: controller.IsAnyData(initial_sheet))
+analyze_button.grid(row=1, column=0)
+
 def main():
     # Intiate mainloop
     app.mainloop()
